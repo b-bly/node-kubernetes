d5e86d30760af298ff8f5c2404c83deb67f39c30
b-bly <brendt.bly@outlook.com>
2023-05-09 08:35:58 -0600

    Terraform build def.

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+    default = "us-central1"
+}
+
+variable "github_repository" {
+    default = "node-kubernetes"
+}
+
+variable "github_owner" {
+    default = "b-bly"
+}
+
+variable "github_branch" {
+    default = "^master$"
+}
+
+variable "project" {
+    default = "node-kubernetes"
+}
